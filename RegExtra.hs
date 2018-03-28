8a65ddd1666547bda37122a24147736351cc2fb0
jkuklis <jk.kuklis@gmail.com>
2018-03-28 12:43:01 +0200

    change

--- RegExtra.hs
+++ RegExtra.hs
@@ -21,12 +21,15 @@ simpl x = case x of
     Many(y) -> let s = simpl(y) in case s of
         Eps -> s
         Empty -> Eps
-        Many(_) -> s
-        _ -> Many(s)
+        Many _ -> s
+        _ -> Many s
     (l :> r) -> let g = gather_c x [] in if (has_Empty g)
         then Empty
         else roll_c (remove_Eps g)
-    (l :| r) -> let g = nub (gather_a x []) in roll_a (remove_Empty g) 
+        where remove_Eps g = filter (\x -> case x of Eps -> False; otherwise -> True) g
+    (l :| r) -> let g = nub (gather_a x []) in 
+        roll_a (remove_Empty g) 
+        where remove_Empty g = filter (\x -> case x of Empty -> False; otherwise -> True) g
     _ -> x
 
 gather_c :: Eq c => Reg c -> [Reg c] -> [Reg c]
@@ -38,45 +41,23 @@ gather_a :: Eq c => Reg c -> [Reg c] -> [Reg c]
 gather_a x acc = case x of
     (l :| r) -> gather_a l (gather_a r acc)
     _ -> simpl(x) : acc    
-    
+
 has_Empty :: [Reg c] -> Bool
-has_Empty g = case g of
-    [] -> False
-    (x:xs) -> case x of
-        Empty -> True
-        _ -> has_Empty xs   
-
-remove_Eps :: [Reg c] -> [Reg c]
-remove_Eps g = case g of
-    [] -> []
-    (x:xs) -> case x of
-        Eps -> remove_Eps xs
-        _ -> x : (remove_Eps xs)
+has_Empty g = foldr (\x acc -> case x of Empty -> True; _ -> acc) False g
         
-remove_Empty :: [Reg c] -> [Reg c]
-remove_Empty g = case g of
-    [] -> []
-    (x:xs) -> case x of
-        Empty -> remove_Empty xs
-        _ -> x : (remove_Empty xs)
-
 roll_c :: [Reg c] -> Reg c
-roll_c g = case g of
-    [] -> Eps
-    (x:xs) -> case xs of
-        [] -> x
-        _ -> x :> (roll_c xs)
+roll_c [] = Eps
+roll_c (x:[]) = x
+roll_c (x:xs) = foldl' (\y acc -> y :> acc) x xs
         
 roll_a :: [Reg c] -> Reg c
-roll_a g = case g of
-    [] -> Empty
-    (x:xs) -> case xs of
-        [] -> x
-        _ -> x :| (roll_a xs)        
+roll_a [] = Empty
+roll_a (x:[]) = x
+roll_a (x:xs) = foldl' (\y acc -> y :| acc) x xs
 
 nullable :: Reg c -> Bool
 nullable x = case x of
-    Many(_) -> True
+    Many _ -> True
     Empty -> False
     Eps -> True
     Lit c -> False
@@ -84,32 +65,26 @@ nullable x = case x of
     (x :> y) -> (nullable x) && (nullable y)
 
 empty :: Eq c => Reg c -> Bool 
-empty r = case simpl(r) of
-    Empty -> True
-    _ -> False    
+empty r = (simpl(r) == Empty)   
 
 der :: Eq c => c -> Reg c -> Reg c
 der c r = case r of
     Lit d -> if d == c then Eps else Empty
     Eps -> Empty
     Empty -> Empty
-    Many(y) -> simpl((der c y) :> Many(y))
+    Many y -> simpl((der c y) :> (Many y))
     (x :| y) -> simpl((der c x) :| (der c y))
     (x :> y) -> if (nullable x)
         then simpl(((der c x) :> y) :| (der c y))
         else simpl((der c x) :> y)
 
 ders :: Eq c => [c] -> Reg c -> Reg c
-ders c r = case c of
-    [] -> r
-    (x:xs) -> ders xs (der x r)
+ders [] r = r
+ders (x:xs) r = ders xs (der x r)
 
 accepts :: Eq c => Reg c -> [c] -> Bool
-accepts r w = if w == [] 
-    then False 
-    else if nullable (ders w r)
-        then True
-        else False
+accepts r [] = False
+accepts r w = nullable (ders w r)
 
 mayStart :: Eq c => c -> Reg c -> Bool
 mayStart c r = (accepts r [c]) || not(empty (der c r)) 
@@ -120,30 +95,27 @@ match r w = case (match_h r w [] Nothing) of
     Just p -> Just (reverse p)
 
 match_h :: Eq c => Reg c -> [c] -> [c] -> Maybe [c] -> Maybe[c]
-match_h r w acc prev =
-    let prev' = if nullable r then Just acc else prev in
-        case w of
-            [] -> prev'
-            (x:xs) -> match_h (der x r) xs (x : acc) prev'     
+match_h r w acc prev = case w of
+        [] -> prev'
+        (x:xs) -> match_h (der x r) xs (x : acc) prev' 
+    where prev' = if nullable r then Just acc else prev
 
 search :: Eq c => Reg c -> [c] -> Maybe [c]
-search r w = 
-    if check_eps r w then Just [] else
-        case w of
-        [] -> if nullable r 
-            then Just [] 
-            else Nothing
-        (x:xs) -> let m = match r w in case m of
-            Nothing -> search r xs
-            _ -> m
+search r w = if check_eps r w 
+    then Just []
+    else search_h r w
+            
+search_h :: Eq c => Reg c -> [c] -> Maybe [c]
+search_h r [] = if nullable r 
+    then Just []
+    else Nothing
+search_h r w@(x:xs) = let m = match r w in case m of
+    Nothing -> search_h r xs
+    _ -> m            
 
 check_eps :: Eq c => Reg c -> [c] -> Bool
-check_eps r w =
-    case w of
-        [] -> False
-        (x:xs) -> if (nullable r) && (match r w == Nothing)
-            then True
-            else False
+check_eps r [] = False
+check_eps r w@(x:xs) = (nullable r) && (match r w == Nothing)
 
 findall :: Eq c => Reg c -> [c] -> [[c]]
 findall r w = let rev = reverse (findall_h r w [] (-1)) in 
